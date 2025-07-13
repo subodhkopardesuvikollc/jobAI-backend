9744a76804772fce5ee26724a4bf2a2970980219
Dheeraj Krishna Nagula <dheerajkrishna141@gmail.com>
2025-07-13 18:10:56 -0400

    Added Cors config.

---
+++ src/main/java/com/suvikollc/resume_rag/configuration/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.suvikollc.resume_rag.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+	
+	@Value("${cors.allowed.origins:http://localhost:3000}")
+	private String allowedOrigins;
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins(allowedOrigins.split(","))
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true);
+	}
+	
+	
+	
+
+}
