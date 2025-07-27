ec1b01aee69268c8cfe17a6a7de4186a6e2dcf4b
Dheeraj Krishna Nagula <dheerajkrishna141@gmail.com>
2025-07-27 12:07:51 -0400

    Implemented following:
    - Added method to extract the candidate email ID before persisting the
    resume in storage.

--- src/main/java/com/suvikollc/resume_rag/entities/Resume.java
+++ src/main/java/com/suvikollc/resume_rag/entities/Resume.java
@@ -1,11 +1,11 @@
 package com.suvikollc.resume_rag.entities;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,8 +13,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
 @Document(collection = "resumes")
 public class Resume extends File {
+	
+	private String emailId;
 
 	public Resume(ObjectId id, String fileName, String blobName, LocalDateTime createdAt, LocalDateTime updatedAt) {
 		super(id, fileName, blobName, createdAt, updatedAt);
